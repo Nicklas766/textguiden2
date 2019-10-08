75468136f1b0cd1254e3c81ec8827a03912cb832
Nicklas766 <Nicklas766@live.se>
2019-10-08 17:53:46 +0200

    fixed accessibility for search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,29 +4,32 @@ import Router from 'next/router';
 import SearchIcon from '@material-ui/icons/Search';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import FormLabel from '@material-ui/core/FormLabel';
 
 const SearchBar: React.FunctionComponent = () => {
     const [text, setText] = useState('');
 
     return (
-        <div>
+        <FormLabel>
             <TextField
                 onChange={(event) => setText(event.target.value)}
                 value={text}
                 fullWidth
                 variant="outlined"
                 label="Sök efter ett ord"
+                type='input'
             />
 
             <Button
                 onClick={() => Router.push('/ord/[word]', `/ord/${text.toLowerCase()}`, { shallow: true })}
                 fullWidth
+                aria-label="search"
                 color="default"
                 style={{ background: '#15C39A', color: 'white', marginTop: '12px' }}
             >
                 <SearchIcon />
             </Button>
-        </div>
+        </FormLabel>
     )
 }
 
