91508213b0744a84b9b8d1dbd344b03757a82a3d
Nicklas766 <Nicklas766@live.se>
2019-11-20 22:22:25 +0100

    added polyfills so /spel route works for IE11

--- next.config.js
+++ next.config.js
@@ -7,6 +7,17 @@ module.exports = {
       new webpack.EnvironmentPlugin(process.env)
     )
 
+    const originalEntry = config.entry;
+    config.entry = async () => {
+        const entries = await originalEntry();
+
+        if (entries['main.js'] && !entries['main.js'].includes('./polyfills.js')) {
+            entries['main.js'].unshift('./polyfills.js');
+        }
+
+        return entries;
+    };
+
     return config;
   },
   experimental: { publicDirectory: true }
